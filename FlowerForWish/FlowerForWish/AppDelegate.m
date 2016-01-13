177bca1944bbd930ab0be951d3e3c2bc8eb393a6
kt <812144991@qq.com>
2016-01-13 16:53:47 +0800

    //landingpage

--- FlowerForWish/FlowerForWish/AppDelegate.m
+++ FlowerForWish/FlowerForWish/AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
-#import "RootViewController.h"
+#import "LandingpageViewController.h"
+
 @interface AppDelegate ()
 
 @end
@@ -17,18 +18,23 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    self.window =[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.backgroundColor = [UIColor whiteColor];
+    //设置NavigationBar背景颜色
+    //[[UINavigationBar appearance] setBarTintColor:[UIColor whiteColor]];
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    LandingpageViewController *login=[[LandingpageViewController alloc]init];
+    
+    self.window.rootViewController=login;
     [self.window makeKeyAndVisible];
-    RootViewController *root = [[RootViewController alloc]init];
-    self.window.rootViewController = root;
+    //
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
 }
+
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
